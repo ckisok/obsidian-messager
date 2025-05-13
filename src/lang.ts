401dfa96ba94066b277772c70b364a521cf75ef2
luoyunan <xiaotianhu2@gmail.com>
2025-05-13 11:46:39 +0800

    支持模板；支持Email下载内嵌图片

--- src/lang.ts
+++ src/lang.ts
@@ -58,6 +58,7 @@ Supports date variables (must be enclosed in curly braces), prefix/suffix suppor
 
     LATEST_UPDATE: string = `
 UpdateNotes:
+    (v1.1.3@25-05-13): Support template
     (v1.1.2@25-01-20): Fix bugs.
     (v1.1.0@24-09-09): Fix bugs.
     (v1.0.9@24-09-07): Support date variables for fixed title.
@@ -128,9 +129,9 @@ UpdateNotes:
 
         this.LATEST_UPDATE = `
 更新记录：
+(v1.1.3@25-05-13): 支持模板消息；支持Email内嵌图片下载
 (v1.1.2@25-01-20): Fix bugs 设置标题的Bug 
 (v1.1.0@24-09-09): Fix bugs.
-(v1.0.9@24-09-07): 固定标题支持使用时间戳变量。
 `
 	}
 }
